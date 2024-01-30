5e623030e9411975fe933d71c97037f242c70c16
Alec McCutcheon <alec@obc.tech>
2024-01-30 14:21:19 -0500

    Update

--- LODCP.js
+++ LODCP.js
@@ -47,6 +47,6 @@ async function StartLODCP (fullUrl, customDNSResolver, port) {
 };
 
 // Example usage:
-const TargetURL = 'https://www.wmtw.com/nowcast/status';
+const TargetURL = 'https://example.com';
 
 StartLODCP(TargetURL, '1.1.1.1', 7007).then(data => {console.log(data)}).catch();
